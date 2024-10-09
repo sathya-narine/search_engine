beb34e58eb56df026f060b911dba420ea3d3616b
sathya-narine <sathyanarine21@gmail.com>
2024-10-09 12:49:15 -0400

    initial files

---
+++ search_engine.py
@@ -0,0 +1,7 @@
+# search engine 
+# Datasets -> iterate through datadset over al documents
+# For each documents do the following
+#     for each line break into individual terms - tokenization
+#     apply stemming, remove the stop words
+#     count the frequency and create a hash map
+#     invert the index
